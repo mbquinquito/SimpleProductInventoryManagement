57418137508ae47beaff59ddc6725ec0a9a2e8e8
mbquinquito <mbquinquito@premierse.com.ph>
2025-08-13 13:26:13 +0800

    Final

--- SimpleProductInventoryManagement/SimpleProductInventoryManagement.PLEASE/Contracts/IAuthenticationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimpleProductInventoryManagement.BlazorUI.Contracts
-{
-    public interface IAuthenticationService
-    {
-        Task<bool> AuthenticateAsync(string email, string password);
-
-        Task Logout();
-    }
-}
